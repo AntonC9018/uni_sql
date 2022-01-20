7c37411ba3953099114d05a6e0693155409723ea
AntonC9018 <anton.curm@gmail.com>
2022-01-20 20:29:55 +0200

    the procedure worked

--- sem6/lab3/scripts/add_login_and_assembly.sql
+++ sem6/lab3/scripts/add_login_and_assembly.sql
@@ -1,27 +1,35 @@
-USE master;
-GO
+use master;
+go
 
 create or alter procedure [dbo].[DoAssemblyStuff](
-    @project_name varchar(max),
+    @assembly_name varchar(max),
     @assembly_full_path varchar(max))
 as
 begin
-    declare @key_name   varchar(max) = @project_name + '_Key'
-    declare @login_name varchar(max) = @project_name + '_Login'
+    declare @key_name   varchar(max) = @assembly_name + '_Key'
+    declare @login_name varchar(max) = @assembly_name + '_Login'
 
-    -- Create asymmetric key
     declare @create_key_command_text varchar(max) = formatmessage(
-        'create asymmetric key %s from executable file = ''%s''',
+        'create asymmetric key %s 
+        from executable file = ''%s''',
         @key_name, @assembly_full_path)
 
-    -- Create a new login with the asymmetric key
     declare @create_login_command_text varchar(max) = formatmessage(
-        'create login %s from asymmetric key %s',
+        'create login %s 
+        from asymmetric key %s',
         @login_name, @key_name)
 
-    -- Grant External access to login
     declare @grant_external_access_command_text varchar(max) = formatmessage(
         'grant external access assembly to %s', @login_name)
+    
+	declare @grant_unsafe_assembly_command_text varchar(max) = formatmessage(
+		'grant unsafe assembly to %s', @login_name)
+
+    declare @create_assembly_command_text varchar(max) = formatmessage(
+		'create assembly %s
+        from ''%s''
+        with permission_set = SAFE', @assembly_name, @assembly_full_path)
+		
 
     declare @is_debug int = 1
 
@@ -30,18 +38,22 @@ begin
         print @create_key_command_text
         print @create_login_command_text
         print @grant_external_access_command_text
+        print @grant_unsafe_assembly_command_text
+        print @create_assembly_command_text
     end
     begin
         exec(@create_key_command_text)
         exec(@create_login_command_text)
         exec(@grant_external_access_command_text)
+        exec(@grant_unsafe_assembly_command_text)
+        exec(@create_assembly_command_text)
     end
 end
 go
 
 
-declare @project_name   varchar(max) = 'TestProject'
-declare @assembly_path  varchar(max) = 'E:\Coding\uni_sql\sem6\lab3\assembly\bin\Debug\net461\Test.dll'
+declare @assembly_name varchar(max) = 'TestProject'
+declare @assembly_path varchar(max) = 'E:\Coding\uni_sql\sem6\lab3\assembly\bin\Debug\net461\Test.dll'
 
-exec DoAssemblyStuff @project_name, @assembly_path
+exec DoAssemblyStuff @assembly_name, @assembly_path
 go
