0b45500287812bba61f8b6e8a38263492db36ba1
AntonC9018 <anton.curm@gmail.com>
2022-01-20 23:01:46 +0200

    debugger no luck

--- sem6/lab3/assembly/test.cs
+++ sem6/lab3/assembly/test.cs
@@ -1,10 +1,35 @@
 using System;
 using Microsoft.SqlServer.Server;
+using System.Diagnostics;
 
 public class Test
 {
     public static void Stuff()
     {
         SqlContext.Pipe.Send("test" + Environment.NewLine);
+
+        if (SqlContext.TriggerContext is null)
+            return;
+
+        var triggerContext = SqlContext.TriggerContext;
+
+        switch (triggerContext.TriggerAction)
+        {
+            case TriggerAction.Insert:
+            {
+                SqlContext.Pipe.Send(triggerContext.EventData.Value);
+                break;
+            }
+            
+            case TriggerAction.Update:
+            {
+                break;
+            }
+            
+            case TriggerAction.Delete:
+            {
+                break;
+            }
+        }
     }
 }
